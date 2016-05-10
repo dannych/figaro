bd3f3bb879ae88064a2ce17486637f46b6ca46a2
Danny Christanto <danny.christanto@gdplabs.id>
2016-05-10 19:23:14 +0700

    refactor

---
+++ webhooks/index.js
@@ -0,0 +1,37 @@
+var moment = require('moment'),
+  _ = require('lodash'),
+  customerService = require('../library/customer-service');
+
+var todayReminderTime = moment('08:00:00 +0700','HH:mm:ss ZZ'),
+  tomorrowReminderTime = moment('17:00:00 +0700','HH:mm:ss ZZ'),  
+  channel = 'standup'
+    
+module.exports = function(bot, controller) {
+  setInterval(_.partials(sendWebhook,bot), 1000);    
+};
+
+function sendWebhook(bot) {
+  var now = moment();    
+  todayReminder(bot,now);
+  tomorrowReminder(bot,now);
+}
+
+function todayReminder(bot, now) {
+  if (now.utc().hour() !== todayReminderTime.utc().hour() || now.utc().minute() !== todayReminderTime.utc().minute() || now.utc().second() != todayReminderTime.utc().second()) return;
+          
+  bot.sendWebhook({
+    text: 'Hi @' + customerService.getTodayCs() + ', this is just a friendly reminder. Today, you will be in charge as Costumer Service',
+    link_names: 1,
+    channel: channel,
+  });  
+}
+
+function tomorrowReminder(bot, now) {
+  if (now.utc().hour() !== tomorrowReminderTime.utc().hour() || now.utc().minute() !== tomorrowReminderTime.utc().minute() || now.utc().second() != tomorrowReminderTime.utc().second()) return;
+          
+  bot.sendWebhook({
+    text: 'Hi @' + customerService.getTomorrowCs() + '! Tomorrow, you will be in charge as Costumer Service',
+    link_names: 1,
+    channel: channel,
+  }); 
+}

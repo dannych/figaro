df7ebc7c0aa9625b3101d033117581cd52d11935
Danny Christanto <danny.christanto@gdplabs.id>
2016-05-11 22:14:09 +0700

    fix error

--- config/index.js
+++ config/index.js
@@ -1,9 +1,8 @@
-var secret = require('./variables/secret.json'),
-    custom = require('./variables/custom.json');
+var custom = require('./variables/custom.json');
 
 module.exports = {
-  SLACK_BOT_TOKEN     : process.env.SLACK_BOT_TOKEN   || secret.SLACK_BOT_TOKEN,
-  SLACK_WEBHOOK_URL   : process.env.SLACK_WEBHOOK_URL || secret.SLACK_WEBHOOK_URL,
+  SLACK_BOT_TOKEN     : process.env.SLACK_BOT_TOKEN   || require('./variables/secret.json').SLACK_BOT_TOKEN,
+  SLACK_WEBHOOK_URL   : process.env.SLACK_WEBHOOK_URL || require('./variables/secret.json').SLACK_WEBHOOK_URL,
   SLACK_CHANNEL       : process.env.CHANNEL           || custom.CHANNEL,
 
   BOT_WEBSERVER_PORT  : process.env.PORT              || custom.PORT,

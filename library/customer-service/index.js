9f0eefb0849bd4f76c34fc014b8523b40e6be1b9
Danny Christanto <danny.christanto@gdplabs.id>
2016-05-11 21:49:24 +0700

    revert

--- library/customer-service/index.js
+++ library/customer-service/index.js
@@ -34,9 +34,9 @@ function getNextCs() {
   return personInCharge;      
 }
 
-function getDiffFromNow(start) {
-  return start.utc().isoWeekdayCalc({  
-    rangeEnd: moment().utc(),  
+function getDiffFromNow(end) {
+  return moment().isoWeekdayCalc({  
+    rangeEnd: end,  
     weekdays: [1,2,3,4,5],  
     // exclusions: ['6 Apr 2015','7 Apr 2015'],
     // inclusions: ['10 Apr 2015']

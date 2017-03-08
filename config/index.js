d6291fc17148d033b12183c82414592a5ca11819
dannych <dnny.ch@gmail.com>
2017-03-08 13:39:05 +0700

    remove webhook token

--- config/index.js
+++ config/index.js
@@ -2,7 +2,6 @@ var custom = require('./variables/custom.json');
 
 module.exports = {
   SLACK_BOT_TOKEN     : process.env.SLACK_BOT_TOKEN   || require('./variables/secret.json').SLACK_BOT_TOKEN,
-  SLACK_WEBHOOK_URL   : process.env.SLACK_WEBHOOK_URL || require('./variables/secret.json').SLACK_WEBHOOK_URL,
   SLACK_CHANNEL       : process.env.CHANNEL           || custom.CHANNEL,
 
   BOT_WEBSERVER_PORT  : process.env.PORT              || custom.PORT,
